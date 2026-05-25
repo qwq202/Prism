1812a791b1f18cfb6e08dcf497675d4827a4e04a
qunqin <146629280+qwq202@users.noreply.github.com>
2026-05-25 13:29:55 +0800

    fix: avoid duplicate app bootstrap

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -4,6 +4,5 @@ import "./conf/bootstrap.ts";
 import "./i18n.ts";
 import "./assets/main.less";
 import "./assets/globals.less";
-import "./conf/bootstrap.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
